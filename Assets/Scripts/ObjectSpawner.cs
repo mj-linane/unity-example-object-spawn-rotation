d322bce79766935b9d4bbea359483d5c3b5cca9e
Michael Linane <mjlinane@ORRHS.ORG>
2021-03-29 11:20:15 -0400

    Initial commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+        } 
+    }
+}
